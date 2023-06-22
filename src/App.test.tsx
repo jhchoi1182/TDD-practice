f4312e0f4dff1fe9a69b03a9f8f0ec233e174046
jhchoi1182 <jhchoi1182@gmail.com>
2023-06-22 10:19:01 +0900

    첫 TDD!

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+// 버튼 여부와 텍스트가 "파란색으로 변경"인지 확인
+test("버튼의 초기 색상이 올바른지 확인하고 클릭 시 업데이트", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const colorButton = screen.getByRole("button", { name: "파란색으로 변경" });
+  expect(colorButton).toHaveStyle("background-color: red");
+  fireEvent.click(colorButton);
+  expect(colorButton).toHaveStyle("background-color: blue");
+  expect(colorButton).toHaveTextContent("빨간색으로 변경")
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [isRed, setIsRed] = useState(true);
+  const newButtonColor = isRed ? "red" : "blue";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button style={{ backgroundColor: newButtonColor }} onClick={() => setIsRed((prev) => !prev)}>
+        {isRed ? "파란색으로 변경" : "빨간색으로 변경"}
+      </button>
     </div>
   );
 }
-
-export default App;
