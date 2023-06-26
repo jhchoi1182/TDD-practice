1dfb5efa9d38304fc3b93504b8ed4d235cc86c87
jhchoi1182 <jhchoi1182@gmail.com>
2023-06-26 12:32:55 +0900

    토핑 업데이트 테스트

--- src/pages/entry/ToppingOption.tsx
+++ src/pages/entry/ToppingOption.tsx
@@ -1,10 +1,20 @@
 import Col from "react-bootstrap/Col";
 import { IScoopOption } from "./ScoopOption";
+import { useOrderDetails } from "../../contexts/OrderDetails";
+import { Form } from "react-bootstrap";
 
 export default function ToppingOption({ name, imagePath }: IScoopOption) {
+  const { updateItemCount } = useOrderDetails();
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    updateItemCount(name, e.target.checked ? 1 : 0, "toppings");
+  };
+
   return (
     <Col xs={6} sm={4} md={3} lg={2} style={{ textAlign: "center" }}>
       <img style={{ width: "75%" }} src={`http://localhost:3030/${imagePath}`} alt={`${name} toppings`} />
+      <Form.Group controlId={`${name}-topping-checkbox`}>
+        <Form.Check type="checkbox" onChange={handleChange} label={name} />
+      </Form.Group>
     </Col>
   );
 }
--- src/pages/entry/test/totalUpdates.test.tsx
+++ src/pages/entry/test/totalUpdates.test.tsx
@@ -23,3 +23,24 @@ test("스쿱 변경되면 업데이트되는지", async () => {
   await user.type(chocolateInput, "2");
   expect(scoopsSubtotal).toHaveTextContent("6.00");
 });
+
+test("토핑 변경되면 업데이트되는지", async () => {
+  const user = userEvent.setup();
+  render(<Options optionType="toppings" />);
+
+  const toppingsTotal = screen.getByText("Toppings total: $", { exact: false });
+  expect(toppingsTotal).toHaveTextContent("0.00");
+
+  const cherriesCheckbox = await screen.findByRole("checkbox", {
+    name: "Cherries",
+  });
+  await user.click(cherriesCheckbox);
+  expect(toppingsTotal).toHaveTextContent("1.50");
+
+  const hotFudgeCheckbox = screen.getByRole("checkbox", { name: "Hot fudge" });
+  await user.click(hotFudgeCheckbox);
+  expect(toppingsTotal).toHaveTextContent("3.00");
+
+  await user.click(hotFudgeCheckbox);
+  expect(toppingsTotal).toHaveTextContent("1.50");
+});
